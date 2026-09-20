a2f1f629f740d230a37411224bd2c12eb9746ae0
andywijaya15 <andykevonata@gmail.com>
2026-09-20 21:08:39 +0700

    fix: Customer phone validation regex delimiter issue
    - Use # as regex delimiter instead of / to avoid conflict with | in pattern
    - Apply to both store and update methods

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -36,7 +36,7 @@ class CustomerController extends Controller
     {
         $validated = $request->validate([
             'name' => 'required|string|max:255',
-            'phone' => 'required|string|max:20|regex:/^(?:\+62|0)8[1-9][0-9]{7,10}$/',
+            'phone' => ['required', 'string', 'max:20', 'regex:#^(?:\+62|0)8[1-9][0-9]{7,10}$#'],
             'address' => 'nullable|string|max:500',
         ]);
 
@@ -59,7 +59,7 @@ class CustomerController extends Controller
     {
         $validated = $request->validate([
             'name' => 'required|string|max:255',
-            'phone' => 'required|string|max:20|regex:/^(?:\+62|0)8[1-9][0-9]{7,10}$/',
+            'phone' => ['required', 'string', 'max:20', 'regex:#^(?:\+62|0)8[1-9][0-9]{7,10}$#'],
             'address' => 'nullable|string|max:500',
         ]);
 
